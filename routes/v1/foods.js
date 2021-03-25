be3cd9a0906fa62f8538b1f28db5adaaf7fa9513
anjovisk <anjovisk@gmail.com>
2021-03-25 11:14:53 -0300

    Criar API para exclusão de prato
    A API para exclusão de pratos foi criada sem nenhuma validação.

--- routes/v1/foods.js
+++ routes/v1/foods.js
@@ -2,12 +2,14 @@ var express = require('express');
 var router = express.Router();
 let foodsMock = require('../../mock/foods');
 
+let foods = foodsMock.foods.slice();
+
 router.get('/', function(req, res, next) {
-  res.send(foodsMock.foods);
+  res.send(foods);
 });
 
 router.get('/:id', function(req, res, next) {
-  let food = foodsMock.foods.find(food => food.id == req.params.id);
+  let food = foods.find(food => food.id == req.params.id);
   if (!food) {
     res.sendStatus(404);
   } else {
@@ -15,9 +17,19 @@ router.get('/:id', function(req, res, next) {
   }
 });
 
+router.delete('/:id', function(req, res, next) {
+  let index = foods.findIndex(food => food.id == req.params.id);
+  if (index === -1) {
+    res.sendStatus(404);
+  } else {
+    foods.splice(index, 1);
+    res.sendStatus(200);
+  }
+});
+
 router.post('/:id/status', function(req, res, next) {
-  let food = foodsMock.foods.find(food => food.id == req.params.id);
-  if (food) {
+  let food = foods.find(food => food.id == req.params.id);
+  if (!food) {
     res.sendStatus(404);
   } else {
     food.status = req.body;
